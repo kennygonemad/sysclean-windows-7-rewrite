3860f85cabc6d7591b661921bd7ff39380ece527
kennygar <kenny.gareth@gmail.com>
2015-04-30 14:10:48 +1000

    version update

--- SysClean/My Project/AssemblyInfo.vb
+++ SysClean/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.6.22")> 
-<Assembly: AssemblyFileVersion("1.0.6.22")> 
+<Assembly: AssemblyVersion("1.1.0.0")> 
+<Assembly: AssemblyFileVersion("1.1.0.0")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
